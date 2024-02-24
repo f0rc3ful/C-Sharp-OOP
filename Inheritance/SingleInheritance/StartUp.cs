7551a4af88e0b1800ff7d922e52d5991a0d2b1c0
marin <f0rc3ful@protonmail.com>
2024-02-24 18:33:58 +0200

    1. 100/100

--- Inheritance/SingleInheritance/StartUp.cs
+++ Inheritance/SingleInheritance/StartUp.cs
@@ -1,6 +1,12 @@
-﻿namespace Farm
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm
 {
-    class Program
+    public class StartUp
     {
         static void Main()
         {
