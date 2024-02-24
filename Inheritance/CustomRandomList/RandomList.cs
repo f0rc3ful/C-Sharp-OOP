fb5a6d878c6166acafccd02c03a4a58fa128e1e7
marin <f0rc3ful@protonmail.com>
2024-02-24 19:02:21 +0200

    custom list first submission

---
+++ Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList
+    {
+		private List<string> CustomList;
+
+		public List<string> CustomList
+		{
+			get { return CustomList; }
+			set { CustomList = value; }
+		}
+		public string RandomString()
+		{
+			Random random = new Random();
+			int randomIndex = random.Next(CustomList.Count - 1);
+			string output = CustomList[randomIndex];
+			CustomList.RemoveAt(randomIndex);
+			return output;
+		}
+
+	}
+}
