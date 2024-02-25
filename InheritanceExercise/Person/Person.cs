6dc368ba4d1321bc773b17956682bdb839325fa9
marin <f0rc3ful@protonmail.com>
2024-02-25 12:44:47 +0200

    initiated the person class

---
+++ InheritanceExercise/Person/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		public Person(string name, int age)
+		{
+			Name = name;
+			Age = age;
+		}
+        public override string ToString()
+        {
+            return $"Name: {name}, Age: {age}";
+        }
+    }
+}
