8fab7dc8f5e10d9dd409c18d64d577ef850dfa52
marin <f0rc3ful@protonmail.com>
2024-02-25 13:47:59 +0200

    override fuelConsumption

---
+++ InheritanceExercise/NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        public override double FuelConsumption 
+        { 
+            get => base.FuelConsumption; 
+            set => base.FuelConsumption = 8; 
+        }
+
+        public RaceMotorcycle(int horsePower, int fuel) : base(horsePower, fuel)
+        {
+
+        }
+    }
+}
