d7395d76f480715519d714f500cc0a19f02df8c1
marin <f0rc3ful@protonmail.com>
2024-02-25 12:38:02 +0200

    Inheritance exercise start

--- Inheritance/CustomStack/CustomStack.cs
+++ Inheritance/CustomStack/CustomStack.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CustomStack
 {
-    internal class CustomStack
+    public class CustomStack : Stack<string>
     {
+
+        public bool IsEmpty() => Count == 0;
+        public void AddRange(IEnumerable<string> input)
+        {
+            foreach (string element in input)
+            {
+                Push(element);
+            }
+        }
     }
 }
