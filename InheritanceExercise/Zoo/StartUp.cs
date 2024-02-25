356ad2ec7d4bf2d795686346edaa0611f26c89b0
marin <f0rc3ful@protonmail.com>
2024-02-25 13:33:42 +0200

    added System. to terminal commands

--- InheritanceExercise/Zoo/StartUp.cs
+++ InheritanceExercise/Zoo/StartUp.cs
@@ -5,9 +5,9 @@
         public static void Main(string[] args)
         {
             Lizard lizard = new Lizard("Lizzo");
-            Console.WriteLine($"Lizard's name: {lizard.Name}");
+            System.Console.WriteLine($"Lizard's name: {lizard.Name}");
             Bear beer = new Bear("beer");
-            Console.WriteLine($"Bear's name: {beer.Name}");
+            System.Console.WriteLine($"Bear's name: {beer.Name}");
         }
     }
 }
