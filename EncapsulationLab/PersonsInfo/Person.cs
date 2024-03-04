2f42d9c3b91e3463703f78bd3a2b1e0fe6b0d2b5
marin <f0rc3ful@protonmail.com>
2024-03-04 13:31:13 +0200

    persons info 100/100

---
+++ EncapsulationLab/PersonsInfo/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+		private string firstName;
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+		private string lastName;
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+		public Person(string first, string last, int age)
+		{
+			FirstName = first;
+			LastName = last;
+			Age = age;
+		}
+        public override string ToString()
+        {
+            return $"{firstName} {lastName} is {age} years old.";
+        }
+    }
+}
