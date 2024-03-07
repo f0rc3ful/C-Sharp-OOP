0bc5422affe89a2ae34217c2e942824f8ad51239
marin <f0rc3ful@protonmail.com>
2024-03-08 01:07:02 +0200

    last

--- EncapsulationExercise/ShoppingSpree/Program.cs
+++ EncapsulationExercise/ShoppingSpree/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ShoppingSpree
+{
+    class Program
+    {
+        List<string> allPeople = Console.ReadLine().Split(";").ToList();
+        List<Person> people = new List<Person>();
+        List<string> allProducts = Console.ReadLine().Split(";").ToList();
+    }
+}
