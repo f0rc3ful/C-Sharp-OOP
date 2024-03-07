68f825dc256b662ef926f2d13afd42b67d572a96
marin <f0rc3ful@protonmail.com>
2024-03-08 00:26:16 +0200

    3.

--- EncapsulationExercise/ShoppingSpree/Product.cs
+++ EncapsulationExercise/ShoppingSpree/Product.cs
@@ -8,6 +8,34 @@ namespace ShoppingSpree
 {
     public class Product
     {
+		private string name;
 
-    }
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		private int cost;
+
+		public int Cost
+		{
+			get { return cost; }
+			set 
+            {
+                try
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentException($"Money cannot be negative");
+                    }
+                    cost = value;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+		}
+
+	}
 }
