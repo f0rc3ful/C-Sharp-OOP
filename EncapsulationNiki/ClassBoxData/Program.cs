79f4ce196dd867bd49a4cfc29f05b7903da085aa
marin <f0rc3ful@protonmail.com>
2024-03-10 13:46:18 +0200

    initiated box class, set private setter for length so only the ctor can set the value for length and added logic which processes the value and throws an exception if necessary

--- EncapsulationNiki/ClassBoxData/Program.cs
+++ EncapsulationNiki/ClassBoxData/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ClassBoxData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(3);
+        }
+    }
+}
