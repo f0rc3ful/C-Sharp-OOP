1a035eef98fba530546900da636b7f9b8e344b61
marin <f0rc3ful@protonmail.com>
2024-03-10 11:08:13 +0200

    min salary

---
+++ EncapsulationNiki/setter/Program.cs
@@ -0,0 +1,34 @@
+﻿
+class Person
+{
+    private int salary;
+
+    public int Salary
+    {
+        get 
+        { 
+            return salary; 
+        }
+        set
+        {
+            if (value < 650)
+            {
+                value = 650;
+            }
+            salary = value;
+        }
+    }
+    public Person()
+    {
+    }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.Salary = 300;
+        Console.WriteLine(person1.Salary);
+    }
+}
