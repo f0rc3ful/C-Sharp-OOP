e6db8f8ca242aac02b61f30e589280b33e40bb81
marin <f0rc3ful@protonmail.com>
2024-03-10 10:57:47 +0200

    start

--- EncapsulationExercise/ShoppingSpree/Person.cs
+++ EncapsulationExercise/ShoppingSpree/Person.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace ShoppingSpree
 {
-    internal class Person
+    public class Person
     {
-    }
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set 
+			{
+					if (string.IsNullOrEmpty(value))
+					{
+						throw new ArgumentException($"Name cannot be empty");
+					}
+					name = value;
+			}
+		}
+		private int money;
+
+		public int Money
+		{
+			get { return money; }
+			set 
+			{
+					if (value < 0)
+					{
+						throw new ArgumentException($"Money cannot be negative");
+					}
+			}
+		}
+		private List<Product> products;
+
+		public List<Product> Products
+		{
+			set
+			{
+                this.products = new List<Products>();
+            }
+		}
+
+	}
 }
