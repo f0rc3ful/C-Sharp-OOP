883028aa51d7b4fe8c8ded1de31542974b028749
marin <f0rc3ful@protonmail.com>
2024-03-10 19:58:48 +0200

    shopping spree progress

---
+++ EncapsulationNiki/ShoppingSpree/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ShoppingSpree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+            string[] products = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string part1 = input[0];
+            }
+        }
+    }
+}
