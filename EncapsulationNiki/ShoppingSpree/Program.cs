ff77fc89a7924937578f676511c01a8460583c60
marin <f0rc3ful@protonmail.com>
2024-03-10 22:49:59 +0200

    progress

--- EncapsulationNiki/ShoppingSpree/Program.cs
+++ EncapsulationNiki/ShoppingSpree/Program.cs
@@ -4,13 +4,38 @@
     {
         static void Main(string[] args)
         {
-            string[] people = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
-            string[] products = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+            char[] delims = new[] { ';' , '=' }; 
+            string[] peopleData = Console.ReadLine().Split(delims, StringSplitOptions.RemoveEmptyEntries);
+            string[] productsData = Console.ReadLine().Split(delims, StringSplitOptions.RemoveEmptyEntries);
+            List<Person> personsList = new List<Person>();
+            List<Product> productsList = new List<Product>();
+
+            try
+            {
+                for (int i = 0; i < peopleData.Length; i += 2)
+                {
+                    string name = peopleData[i];
+                    int money = int.Parse(peopleData[i + 1]);
+                    Person person = new Person(name, money);
+                }
+                for (int j = 0; j < productsData.Length; j++)
+                {
+                    string name = productsData[j];
+                    int cost = int.Parse(productsData[j + 1]);
+                    Person person = new Person(name, cost);
+                }
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
 
             string input;
             while ((input = Console.ReadLine()) != "END")
             {
-                string part1 = input[0];
+                string[] parts = input.Split(" ");
+                string name = parts[0];
+                string product = parts[1];
             }
         }
     }
