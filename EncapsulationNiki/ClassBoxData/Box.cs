62794aca7ee95c0d0936afb791866849337aa1f3
marin <f0rc3ful@protonmail.com>
2024-03-10 14:04:32 +0200

    box done, awaiting submission

--- EncapsulationNiki/ClassBoxData/Box.cs
+++ EncapsulationNiki/ClassBoxData/Box.cs
@@ -6,7 +6,103 @@ using System.Threading.Tasks;
 
 namespace ClassBoxData
 {
-    internal class Box
+    public class Box
     {
+		private double length;
+
+		public double Length
+		{
+			get 
+			{ 
+				return length; 
+			}
+			private set 
+			{
+					if (value <= 0)
+					{
+						throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+					}
+					length = value;
+			}
+		}
+		private double width;
+
+		public double Width
+		{
+			get 
+			{ 
+				return width; 
+			}
+			private set 
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+				}
+				width = value; 
+			}
+		}
+		private double height;
+
+		public double Height
+		{
+			get 
+			{
+				return height; 
+			}
+			set 
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+				}
+				height = value; 
+			}
+		}
+
+
+		public Box(double length, double width, double height)
+		{
+			try
+			{
+                this.Length = length;
+            }
+			catch (ArgumentException exception1)
+			{
+				Console.WriteLine(exception1.Message);
+			}
+			try
+			{
+				this.Width = width;
+			}
+			catch (ArgumentException exception2)
+			{
+				Console.WriteLine(exception2.Message);
+			}
+			try
+			{
+				this.Height = height;
+			}
+			catch (ArgumentException exception3)
+			{
+				Console.WriteLine(exception3.Message);
+			}
+        }
+		public double SurfaceArea()
+		{
+			return 2 * (height * width + height * length + length * width);
+		}
+		public double LateralSurfaceArea()
+		{
+			return 2 * (length * height + width * height);
+		}
+		public double Volume()
+		{
+			return length * height * width;
+		}
+        public override string ToString()
+        {
+			return $"Surface Area - {SurfaceArea():F2}\nLateral Surface Area - {LateralSurfaceArea():F2}\nVolume - {Volume():F2}";
+        }
     }
 }
--- EncapsulationNiki/ClassBoxData/Program.cs
+++ EncapsulationNiki/ClassBoxData/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Box box = new Box(3);
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Box box = new Box(length, width, height);
+            if (!(length <= 0 || width <= 0 || height <= 0))
+            {
+                
+                Console.WriteLine(box);
+            }
         }
     }
 }
