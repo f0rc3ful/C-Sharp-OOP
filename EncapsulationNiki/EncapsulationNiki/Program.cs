14a5c87ea6d5c82e31ff61a108153df0f6905af6
marin <f0rc3ful@protonmail.com>
2024-03-10 11:01:02 +0200

    set a property to be readonly by having a private setter which is accessed only by the class (ctor) and once the firstName value is set for the given instance of a Person, it cannot be accessed or modified

--- EncapsulationNiki/EncapsulationNiki/Program.cs
+++ EncapsulationNiki/EncapsulationNiki/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Person
+{
+	private string firstName;
+
+	public string FirstName
+	{
+		get { return firstName; }
+		private set { firstName = value; }
+	}
+	public Person(string firstName)
+	{
+		this.firstName = firstName;
+	}
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+		Person person1 = new Person("Niki");
+		person1.FirstName = "Kolio";
+    }
+}
