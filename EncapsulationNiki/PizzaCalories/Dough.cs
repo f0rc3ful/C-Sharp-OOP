f4996f789663a50b6ab0ce03c7dd6b7f8864e814
marin <f0rc3ful@protonmail.com>
2024-03-14 18:13:34 +0200

    row

---
+++ EncapsulationNiki/PizzaCalories/Dough.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaCalories
+{
+    public class Dough
+    {
+		private string flourType;
+
+		public string FlourType
+		{
+			get { return flourType; }
+			set { flourType = value; }
+		}
+
+	}
+}
