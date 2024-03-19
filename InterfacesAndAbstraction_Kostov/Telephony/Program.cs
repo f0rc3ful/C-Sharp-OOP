baa98fb1314e1bb444b7f89bb3982e0138903cb7
marin <f0rc3ful@protonmail.com>
2024-03-19 14:56:36 +0200

    3. before submission

--- InterfacesAndAbstraction_Kostov/Telephony/Program.cs
+++ InterfacesAndAbstraction_Kostov/Telephony/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            
+            string[] phoneNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] websites = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> qNumbers = new Queue<string>(phoneNumbers);
+
+            while (qNumbers.Count > 0)
+            {
+                string number = qNumbers.Dequeue();
+                foreach (char c in number)
+                {
+                    if (!char.IsDigit(c))
+                    {
+                        Console.WriteLine("Invalid number!");
+                        continue;
+                    }
+                }
+                if (number.Length == 10)
+                {
+                    Console.WriteLine($"Calling... {number}");
+                }
+                else if (number.Length == 7)
+                {
+                    Console.WriteLine($"Dialing... {number}");
+                }
+            }
+
+            Queue<string> qWebsites = new Queue<string>(websites);
+
+            while (qWebsites.Count > 0)
+            {
+                string website = qWebsites.Dequeue();
+                bool valid = true;
+                foreach (char c in website)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        Console.WriteLine("Invalid URL!");
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid) 
+                {
+                    Console.WriteLine($"Browsing: {website}");
+                }
+
+            }
         }
     }
 }
